23078eaf8b197553131d77032780a2204a823fd8
Aislan Michel Moreira Freitas <aislan.freitas@am4.com.br>
2020-07-24 01:28:57 -0300

    added iuserservice

---
+++ src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces.Services.User
+{
+     public interface IUserInterface
+     {
+         Task<UserEntity> GetUser(long id);
+         Task<IEnumerable<UserEntity>> GetAllUsers();
+         Task<UserEntity> CreateUser(UserEntity user);
+         Task<UserEntity> UpdateUser(UserEntity user);
+         Task<bool> DeleteUser(long id);
+     }
+}
